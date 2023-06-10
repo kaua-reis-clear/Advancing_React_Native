64e75e5a9d6d5f46c2ec821a2b96a8e07669b059
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-06-09 22:01:55 -0300

    feat: created currency conversor app

--- App.js
+++ App.js
@@ -1,50 +1,24 @@
 import React, {Component} from 'react';
-import {View, StyleSheet, FlatList, ActivityIndicator} from 'react-native';
+import {StyleSheet, View, Text} from 'react-native';
 
-import api from './src/services/api';
-import Filmes from './src/Filmes';
+import Conversor from './src/Conversor';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      filmes: [],
-      loading: true,
-    };
-  }
-
-  async componentDidMount() {
-    const response = await api.get('r-api/?api=filmes');
-    this.setState({
-      filmes: response.data,
-      loading: false,
-    });
-  }
-
   render() {
-    if (this.state.loading) {
-      return (
-        <View style={{alignItems: 'center', justifyContent: 'center', flex: 1}}>
-          <ActivityIndicator color="#09A6FF" size={40} />
-        </View>
-      );
-    } else {
-      return (
-        <View style={styles.container}>
-          <FlatList
-            data={this.state.filmes}
-            keyExtractor={item => item.id.toString()}
-            renderItem={({item}) => <Filmes data={item} />}
-          />
-        </View>
-      );
-    }
+    return (
+      <View style={styles.container}>
+        <Conversor moedaA="USD" moedaB="BRL" />
+        <Conversor moedaA="EUR" moedaB="BRL" />
+      </View>
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
